ab94930e82e57f4d721d19d0ffc515abd4969429
Austin Crane <austincrane@gmail.com>
2017-05-08 17:19:05 -0400

    fix colorbox

--- single-portfolio.php
+++ single-portfolio.php
@@ -118,7 +118,7 @@ get_header(); ?>
         
        <div class="item port-image">
        <!--<a class="gallery" href="<?php //echo $large; ?>">-->
-       <a class="port" href="<?php echo $large; ?>" rel="port">
+       <a class="gallery" href="<?php echo $large; ?>" rel="port">
         <img class="<?php echo $imageClass ?>" src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" title="<?php echo $title; ?>" />
         </a>
         <?php //echo custom_field_excerpt(); ?>
